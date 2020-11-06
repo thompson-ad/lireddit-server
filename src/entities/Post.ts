8b483ccd48cec5eb83c83302c85ffe718b58bbd8
Aaron Thompson <aaron.thompson@feedagency.com>
2020-11-06 17:51:06 +0000

    query posts

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
+  @Field() // exposing map to our graphql schema
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
+  @Field() // you can remove this and graphql will not expose it
   @Property({ type: "text" })
   title!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import mikroConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   // create db connection
@@ -13,9 +15,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }), // special object accessible by all resolvers
   });
 
   // create a graphql endpoint for us on express
